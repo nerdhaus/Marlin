14ef67bc79e16991f2271c8f2c2983f467ab4b44
Jeff Eaton <eaton@nerdhaus.net>
2022-02-22 14:54:38 -0600

    Normalizing MESH_INSET, correcting the E3M bed size

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1396,8 +1396,8 @@
 // @section machine
 
 // The size of the printable area
-#define X_BED_SIZE 300
-#define Y_BED_SIZE 300
+#define X_BED_SIZE 310 // Standard spec is 300, but it's really 310; this matters for screw calculations
+#define Y_BED_SIZE 310
 
 // Travel limits (mm) after homing, corresponding to endstop positions.
 #define X_MIN_POS 0
@@ -1663,7 +1663,7 @@
 
   //#define MESH_EDIT_GFX_OVERLAY   // Display a graphics overlay while editing the mesh
 
-  #define MESH_INSET 1              // Set Mesh bounds as an inset region of the bed
+  #define MESH_INSET 10             // Set Mesh bounds as an inset region of the bed
   #define GRID_MAX_POINTS_X 10      // Don't use more than 15 points per axis, implementation limited.
   #define GRID_MAX_POINTS_Y GRID_MAX_POINTS_X
 
