015cd295a223412473e0939e1cbe0cbfa659db92
Jeff Eaton <eaton@nerdhaus.net>
2022-02-21 18:40:31 -0600

    Let’s test a smaller mesh

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1661,8 +1661,9 @@
 
   //#define MESH_EDIT_GFX_OVERLAY   // Display a graphics overlay while editing the mesh
 
-  #define MESH_INSET 1              // Set Mesh bounds as an inset region of the bed
-  #define GRID_MAX_POINTS_X 10      // Don't use more than 15 points per axis, implementation limited.
+  #define MESH_INSET 10              // Set Mesh bounds as an inset region of the bed
+  // Temporarily dialing this down to see if it helps with the dying painfully problem
+  #define GRID_MAX_POINTS_X 6      // Don't use more than 15 points per axis, implementation limited.
   #define GRID_MAX_POINTS_Y GRID_MAX_POINTS_X
 
   //#define UBL_HILBERT_CURVE       // Use Hilbert distribution for less travel when probing multiple points
