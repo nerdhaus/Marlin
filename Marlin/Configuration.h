34ac249dba17d1957edaf0a1599c62676cdbe0bb
Jeff Eaton <eaton@nerdhaus.net>
2022-02-22 22:45:15 -0600

    Stock heated bed PID values

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -641,11 +641,10 @@
   //#define MIN_BED_POWER 0
   //#define PID_BED_DEBUG // Sends debug data to the serial port.
 
-  // 120V 250W silicone heater into 4mm borosilicate (MendelMax 1.5+)
-  // from FOPDT model - kp=.39 Tp=405 Tdead=66, Tc set to 79.2, aggressive factor of .15 (vs .1, 1, 10)
-  #define DEFAULT_bedKp 10.00
-  #define DEFAULT_bedKi .023
-  #define DEFAULT_bedKd 305.4
+  // Stock Ender 3 24V heated bed
+  #define DEFAULT_bedKp 266.28
+  #define DEFAULT_bedKi 43.80
+  #define DEFAULT_bedKd 1079.31
 
   // FIND YOUR OWN: "M303 E-1 C8 S90" to run autotune on the bed at 90 degreesC for 8 cycles.
 #endif // PIDTEMPBED
