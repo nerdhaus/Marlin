b869bb870b920cbeed22e7152a970e24ac93f46e
Jeff Eaton <eaton@nerdhaus.net>
2022-02-22 22:46:13 -0600

    Ender 3 Max Heated bed PID

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -640,12 +640,10 @@
   //#define MIN_BED_POWER 0
   //#define PID_BED_DEBUG // Sends debug data to the serial port.
 
-  // 120V 250W silicone heater into 4mm borosilicate (MendelMax 1.5+)
-  // from FOPDT model - kp=.39 Tp=405 Tdead=66, Tc set to 79.2, aggressive factor of .15 (vs .1, 1, 10)
   // Creality Stock - Ender 3 Max
-  #define DEFAULT_bedKp 162.06
-  #define DEFAULT_bedKi 17.89
-  #define DEFAULT_bedKd 978.75
+  #define DEFAULT_bedKp 142.10
+  #define DEFAULT_bedKi 18.70
+  #define DEFAULT_bedKd 719.99
 
   // FIND YOUR OWN: "M303 E-1 C8 S90" to run autotune on the bed at 90 degreesC for 8 cycles.
 #endif // PIDTEMPBED
